f8a532bc5c0e387e212925d992a7a1f5d5e38eac
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-25 16:30:02 +0500

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Bektemur Mamashayev on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
