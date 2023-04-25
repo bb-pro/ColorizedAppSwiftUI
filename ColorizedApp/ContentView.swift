a3328fb9d8bb1c061c91e5d70a77e1550b1d18fc
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-25 17:30:10 +0500

    work with slider

--- ColorizedApp/ContentView.swift
+++ ColorizedApp/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sampleColor = Color.green
+    @State private var redValue = 0.0
+    @State private var greenValue = 0.0
+    @State private var blueValue = 0.0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Colorized App")
+            Rectangle()
+                .frame(width: 300, height: 150)
+                .cornerRadius(20)
+                .foregroundColor(sampleColor)
+            ColorSliderView(sliderColor: .red, value: $redValue)
+            Spacer()
+            
         }
         .padding()
     }
@@ -22,5 +33,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct ColorSliderView: View {
+    let sliderColor: Color
+    @Binding var value: Double
+    var body: some View {
+        HStack(spacing: 20){
+            Text("0").foregroundColor(.red)
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(sliderColor)
+            Text("\(lround(value))").foregroundColor(.red)
+                .frame(width: 40)
+        }
     }
 }
